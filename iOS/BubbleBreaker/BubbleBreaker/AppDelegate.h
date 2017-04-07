3a3c52a4fbeb97a613dbfefa5e94d453b5f2c8ab
Ian Christie <iankchristie@Ian-Christies-MacBook-Pro-3.local>
2017-04-07 13:15:11 -0700

    Adding a xCode project with Firebase Pods

---
+++ iOS/BubbleBreaker/BubbleBreaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BubbleBreaker
+//
+//  Created by Ian Christie on 4/7/17.
+//  Copyright © 2017 Ian Christie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/BubbleBreaker/BubbleBreaker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BubbleBreaker
+//
+//  Created by Ian Christie on 4/7/17.
+//  Copyright © 2017 Ian Christie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
